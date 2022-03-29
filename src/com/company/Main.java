5d30f0da5160c26e96a006f23e8e1e1227cfbcf2
Guseinov <aydu.776@gmail.com>
2022-03-30 01:09:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int number = 2;
+    switch (number){
+        case 1:
+            System.out.println("Number equals to one");
+            break;
+            case 2:
+            System.out.println("Number equals two");
+            break;
+            case 3:
+            System.out.println("bubu");
+            break;
+        default:
+            System.out.println("number unknown");
+
+    } 
+    }
+}
